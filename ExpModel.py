b54ab5f85dc54cb2226725c20f2a5b160b905c8f
Michelle Spencer <25539767+mrosespencer@users.noreply.github.com>
2018-03-18 20:42:27 -0700

    Added ferry/port data

--- ExpModel.py
+++ ExpModel.py
@@ -9,6 +9,7 @@ name = "times"
 tname = "%s.txt" % name
 ft = open(tname, "r")
 
+# Time data
 arrt = []
 for line in ft:
     line = line.split()
@@ -28,4 +29,18 @@ for i in range(ports):
         small2time[j, i] = arrt[j][i]
     for j in range(ports*2, ports*3):
         largetime[j, i] = arrt[j][i]
+
+
+# Ferry data
+homeport = [1,3,5]
+capacity = [100, 100, 200]
+fuelcost = [400, 400, 600]  #per hour
+portcost = [160, 160, 240]
+
+
+# Port data
+berths = [3, 2, 2, 1, 1]
+porttime = [30, 20, 20, 20, 20]
+
+
 
